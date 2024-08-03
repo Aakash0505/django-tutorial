bd10d82d8d8f864edd4a7731675e01c487ce2633
Aakash <aakashagrawal05052000@gmail.com>
2024-08-03 16:58:18 +0530

    [2024-08-03 16:57] Urls & Views

--- core/core/urls.py
+++ core/core/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from home import views as homeviews
 
 urlpatterns = [
+    path('',homeviews.home,name='home'),
     path('admin/', admin.site.urls),
+    path('success-page/',homeviews.success,name='success')
 ]
